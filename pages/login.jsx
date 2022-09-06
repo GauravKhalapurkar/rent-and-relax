96bde2b6cbe4a4ffdc0c77d7a803d990acc845d4
Gaurav K <gaurav.khalapurkar@gmail.com>
2022-09-06 20:42:38 +0530

    re-installed tailwind

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col min-h-screen bg-slate-900">
+      <div></div>
+    </div>
+  );
+};
+
+export default Login;
